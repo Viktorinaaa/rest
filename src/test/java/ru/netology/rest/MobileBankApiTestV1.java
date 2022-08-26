1475916683a3e46d59780d194f0dbbf09fbf79b8
Viki <masalikavika@yandex.ru>
2022-08-26 12:10:06 +0300

    TestSchemaPass

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,6 +1,9 @@
 package ru.netology.rest;
-
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
 
 import static io.restassured.RestAssured.given;
 
@@ -16,7 +19,23 @@ class MobileBankApiTestV1 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(401);
+          .statusCode(200);
+    }
+
+    @Test
+    void checkSchema() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
+
 
